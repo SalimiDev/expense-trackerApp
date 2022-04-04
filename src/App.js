59d1ad3e4eb79b33eedf40cf59d42fb65facffc6
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-04-04 22:19:03 +0430

    feat:added header & Balance components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
 
 function App() {
-    return <div className='App'>
-      Hello World
-    </div>;
+    return (
+        <div className='App'>
+            <Header />
+            <div className='Container'>
+                <Balance />
+            </div>
+        </div>
+    );
 }
 
 export default App;
