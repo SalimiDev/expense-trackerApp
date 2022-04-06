8cc647ee9b88922928166964692d635634467761
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-04-07 03:25:34 +0430

    feat:added total balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+    const balanceAmount = transactions.map(transaction => transaction.amount);
+    const totalAmount = balanceAmount.reduce((total, number) => total + number, 0).toFixed(2);
+
     return (
         <>
-            <h4 className='font-Lato text-white font-medium' >YOUR BALANCE</h4>
-            <h1 className='font-Lato text-white text-4xl' >$0.00</h1>
+            <h4 className='font-Lato text-white font-medium'>YOUR BALANCE</h4>
+            <h1 className='font-Lato text-white text-4xl'>${totalAmount}</h1>
         </>
     );
 };
