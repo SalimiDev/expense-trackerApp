fc92e2abc9c855394c9ecb0ae706d5d5d90f5339
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-04-07 01:57:58 +0430

    style:added color to show  minus and plus in transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const Transaction = ({ transaction }) => {
+    const { text, amount } = transaction;
+    const sign = amount < 0 ? '-' : '+';
     return (
-        <li className=' w-full bg-white mb-3 p-1 relative flex flex-row justify-between items-center text-gray-600 border-r-4 border-green-400 shadow-lg'>
-            {transaction.text} <span>{transaction.amount}</span>
+        <li
+            className={
+                ' w-full bg-white mb-3 p-1 relative flex flex-row justify-between items-center text-gray-600 border-r-4  shadow-lg ' +
+                (amount < 0 ? 'border-red-600' : 'border-green-500')
+            }>
+            {text}
+            <span>
+                {sign}${Math.abs(amount)}
+            </span>
             <button className='absolute bg-red-500 cursor-pointer w-full h-8  left-0 opacity-0 hover:opacity-60'>X</button>
         </li>
     );
